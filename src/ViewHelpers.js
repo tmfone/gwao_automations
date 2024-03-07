717fd43af8ed775e894549232ff2f662fd7cde37
Thomas Fojan <thomas@tmf.one>
2024-03-07 17:13:11 +0100

    refactor: lint ViewHelpers.js

--- src/ViewHelpers.js
+++ src/ViewHelpers.js
@@ -1,3 +1,4 @@
+/* exported buildErrorCard, buildInfoCard */
 function buildErrorCard(opts) {
   var errorText = opts.errorText;
 
@@ -11,20 +12,20 @@ function buildErrorCard(opts) {
 
   var card = CardService.newCardBuilder();
   card.setHeader(
-      CardService.newCardHeader().setTitle('An unexpected error occurred')
+    CardService.newCardHeader().setTitle('An unexpected error occurred')
   );
   card.addSection(
-      CardService.newCardSection().addWidget(
-          CardService.newTextParagraph().setText(errorText)
-      )
+    CardService.newCardSection().addWidget(
+      CardService.newTextParagraph().setText(errorText)
+    )
   );
 
   if (opts.showStackTrace && opts.exception && opts.exception.stack) {
     var stack = opts.exception.stack.replace(/\n/g, '<br/>');
     card.addSection(
-        CardService.newCardSection()
-            .setHeader('Stack trace')
-            .addWidget(CardService.newTextParagraph().setText(stack))
+      CardService.newCardSection()
+        .setHeader('Stack trace')
+        .addWidget(CardService.newTextParagraph().setText(stack))
     );
   }
 
@@ -35,17 +36,18 @@ function buildInfoCard(opts) {
   var text = opts.text;
   var title = opts.title ? opts.title : trsl('tInfoTitle');
 
-  var card = CardService.newCardBuilder()
-    .setHeader(CardService.newCardHeader()
-    .setTitle(title)
-    .setImageUrl('https://www.gstatic.com/images/icons/material/system/1x/report_black_24dp.png')
-    );
-  
-  card.addSection(
-      CardService.newCardSection()
-      .addWidget(
-          CardService.newTextParagraph().setText(text)
+  var card = CardService.newCardBuilder().setHeader(
+    CardService.newCardHeader()
+      .setTitle(title)
+      .setImageUrl(
+        'https://www.gstatic.com/images/icons/material/system/1x/report_black_24dp.png'
       )
+  );
+
+  card.addSection(
+    CardService.newCardSection().addWidget(
+      CardService.newTextParagraph().setText(text)
+    )
   );
   return card.build();
 }
