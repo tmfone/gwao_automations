9615f4d28fc3138d07002d0311eff34889b4cec5
Thomas Fojan <thomas@tmf.one>
2024-03-07 17:06:02 +0100

    refactor: lint GMailBot

--- src/GmailBot.js
+++ src/GmailBot.js
@@ -1,49 +1,54 @@
+/* global getLabelArray */
+/* exported processLabels */
 const botLabelName = 'Bot';
 
 const accountingFolderId = '10balkKoS1rEazbIKFaEFqUcwv1aFbtKW';
 const dmarcFolderId = '1Yeg_DsJ0ZYTS85uvBBn46D5NY2Cfxzn3';
 
 const processingMap = {
-  "Finance": accountingFolderId,
-  "DMARC": dmarcFolderId}
+  'Finance': accountingFolderId,
+  'DMARC': dmarcFolderId
+};
 
 function processLabels() {
   var botLabel = GmailApp.getUserLabelByName(botLabelName);
   var processedMails = 0;
-  Object.keys(processingMap).forEach( label => {
-    var threads = GmailApp.search('label:'+ label +' !label:'+ botLabelName);
-    threads.forEach(t => {
+  Object.keys(processingMap).forEach((label) => {
+    var threads = GmailApp.search('label:' + label + ' !label:' + botLabelName);
+    threads.forEach((t) => {
       var messageLabels = getLabelArray(t.getLabels());
       var messages = GmailApp.getMessagesForThread(t);
-      messages.forEach(m => {
+      messages.forEach((m) => {
         // it's not necessary to check again for label "Bot" but just to be sure
-        if (!messageLabels.includes(botLabel.getName())){
+        if (!messageLabels.includes(botLabel.getName())) {
           processedMails += storeAttachements(m, processingMap[label]);
         }
-      });  
+      });
     });
   });
-  console.log(processedMails + " Mail(s) processed!");
+  console.log(processedMails + ' Mail(s) processed!');
 }
 
-function storeAttachements(m, folderId){
-  var botLabel = GmailApp.getUserLabelByName(botLabelName);
-  var saveToFolder = DriveApp.getFolderById(folderId);
-  attachments = m.getAttachments();
-  attachments.forEach( a => {
-    if ((a.getContentType() == 'application/octet-stream' && a.getName().endsWith('.pdf')) ||
-        a.getContentType() == 'application/pdf' ||
-        a.getContentType() == 'application/gzip' ||
-        a.getContentType() == 'application/zip'){
+function storeAttachements(m, folderId) {
+  const botLabel = GmailApp.getUserLabelByName(botLabelName);
+  const saveToFolder = DriveApp.getFolderById(folderId);
+  const attachments = m.getAttachments();
+  attachments.forEach((a) => {
+    if (
+      (a.getContentType() == 'application/octet-stream' &&
+        a.getName().endsWith('.pdf')) ||
+      a.getContentType() == 'application/pdf' ||
+      a.getContentType() == 'application/gzip' ||
+      a.getContentType() == 'application/zip'
+    ) {
       // save attachment
       saveToFolder.createFile(a.copyBlob()).setName(a.getName());
-    }
-    else{
-      //skip attachment
-      console.log("Skipped unsupported type: " + a.getContentType());
+    } else {
+      // skip attachment
+      console.log('Skipped unsupported type: ' + a.getContentType());
     }
   });
   m.markRead();
   m.getThread().addLabel(botLabel);
-  return(1);
+  return 1;
 }
