1f4ed67e6a4be6a143b624ae916e2ceb6a0a5aab
Thomas Fojan <thomas@tmf.one>
2024-03-07 17:10:40 +0100

    refactor: PropertyHelpers

--- src/PropertyHelpers.js
+++ src/PropertyHelpers.js
@@ -1,34 +1,35 @@
+/* exported getUserProperty, setUserProperty, deleteUserProperty, getUserProperties, deleteLabelActions */
 
-function getUserProperty(key){
+function getUserProperty(key) {
   try {
     var value = PropertiesService.getUserProperties().getProperties()[key];
   } catch (err) {
     // TODO (developer) - Handle exception
     console.log('Failed with error %s', err.message);
   }
-  return(value)
+  return value;
 }
-function setUserProperty(key, value){
+function setUserProperty(key, value) {
   try {
-      // Set a property in each of the three property stores.
-      const userProperties = PropertiesService.getUserProperties();
-      userProperties.setProperty(key, value);
-    } catch (err) {
-      console.log('Failed with error %s', err.message);
-    }
+    // Set a property in each of the three property stores.
+    const userProperties = PropertiesService.getUserProperties();
+    userProperties.setProperty(key, value);
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
 }
-function deleteUserProperty(key){
+function deleteUserProperty(key) {
   try {
-      // Set a property in each of the three property stores.
-      const userProperties = PropertiesService.getUserProperties();
-      userProperties.deleteProperty(key);
-    } catch (err) {
-      console.log('Failed with error %s', err.message);
-    }
+    // Set a property in each of the three property stores.
+    const userProperties = PropertiesService.getUserProperties();
+    userProperties.deleteProperty(key);
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
 }
 
-/*FOR DEBUGGING*/
-function getUserProperties(){
+/* FOR DEBUGGING*/
+function getUserProperties() {
   try {
     var value = PropertiesService.getUserProperties().getProperties();
     console.log(value);
@@ -36,15 +37,15 @@ function getUserProperties(){
     // TODO (developer) - Handle exception
     console.log('Failed with error %s', err.message);
   }
-  return(value)
+  return value;
 }
-function deleteLabelActions(key){
+function deleteLabelActions(key) {
   key = 'labelActions';
   try {
-      // Set a property in each of the three property stores.
-      const userProperties = PropertiesService.getUserProperties();
-      userProperties.deleteProperty(key);
-    } catch (err) {
-      console.log('Failed with error %s', err.message);
-    }
+    // Set a property in each of the three property stores.
+    const userProperties = PropertiesService.getUserProperties();
+    userProperties.deleteProperty(key);
+  } catch (err) {
+    console.log('Failed with error %s', err.message);
+  }
 }
