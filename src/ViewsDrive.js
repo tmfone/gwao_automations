724c088b408193811739549607642a691a63c5ae
Thomas Fojan <thomas@tmf.one>
2024-03-07 17:14:06 +0100

    refactor: lint ViewsDrive.js

--- src/ViewsDrive.js
+++ src/ViewsDrive.js
@@ -1,39 +1,41 @@
-/*DRIVE*/
+/* exported getDriveCard, getFolderSettingsCard, folderLabelChange */
+/* global buildInfoCard, trsl, constMap, debugFlag */
 
-function getDriveCard(context){
-  if(debugFlag){
-    console.log("function getDriveCard");
+function getDriveCard(context) {
+  if (debugFlag) {
+    console.log('function getDriveCard');
     console.log(context);
     console.log(context.drive);
   }
-  if(context.drive.selectedItems && context.drive.selectedItems.length > 1) {
-    return(buildInfoCard({
+  if (context.drive.selectedItems && context.drive.selectedItems.length > 1) {
+    return buildInfoCard({
       text: trsl('tSelectOnlyOne')
-    })); 
-  }
-  else if (context.drive.selectedItems){
-    context.tItemSelected = true
+    });
+  } else if (context.drive.selectedItems) {
+    context.tItemSelected = true;
     context.tSelectedItem = context.drive.selectedItems[0];
-    context.tSelType = context.tSelectedItem.mimeType  == 'application/vnd.google-apps.folder' ? constMap.selFolder:constMap.selFile;
+    context.tSelType =
+      context.tSelectedItem.mimeType == 'application/vnd.google-apps.folder'
+        ? constMap.selFolder
+        : constMap.selFile;
   }
-  if (context.tItemSelected && context.tSelType == constMap.selFolder){
-    return(getFolderSettingsCard(context));
-  }
-  else{
-    return(buildInfoCard({
+  if (context.tItemSelected && context.tSelType == constMap.selFolder) {
+    return getFolderSettingsCard(context);
+  } else {
+    return buildInfoCard({
       text: trsl('tSelectFolder')
-    })); 
+    });
   }
 }
 
 function getFolderSettingsCard(context) {
-  return(buildInfoCard({
+  return buildInfoCard({
     text: trsl('tFolderActionsUnsupported')
-  })); 
+  });
 }
 
-function folderLabelChange(e){
-  if(debugFlag){
+function folderLabelChange(e) {
+  if (debugFlag) {
     console.log(e);
   }
 }
