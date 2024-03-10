8283f596743dc67fc3f081c82bc221f1a11aba2b
Thomas Fojan <thomas@tmf.one>
2024-03-10 17:57:25 +0100

    feat: add a playground file

---
+++ src/_playground.js
@@ -0,0 +1,15 @@
+/* exported listFilters */
+function listFilters() {
+  var userId = 'me';
+  var response = UrlFetchApp.fetch(
+    'https://www.googleapis.com/gmail/v1/users/' + userId + '/settings/filters',
+    {
+      headers: {
+        'Authorization': 'Bearer ' + ScriptApp.getOAuthToken()
+      },
+      'muteHttpExceptions': true // Optional, prevents exceptions from being thrown for 4xx and 5xx responses
+    }
+  );
+  var filters = JSON.parse(response.getContentText());
+  Logger.log(filters);
+}
